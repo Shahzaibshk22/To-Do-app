188e1507c7913a2352f4636df3b6b8d0636294a7
Shahzaibshk22 <skshahzaib788@gmail.com>
2026-01-12 18:04:39 +0500

    Initial commit - To Do App

---
+++ app1.js
@@ -0,0 +1,50 @@
+const todoList=document.getElementById("todoList")
+const Inputvalue=document.getElementById("todoInput")
+
+function onClickBtn(xyz){
+    xyz.textcontent="text changed"
+}
+function addTodo(ele){
+    let cleanInput=Inputvalue.value
+    if(Inputvalue.value==""){
+        alert("Instruction Invalid")
+    }
+    else {
+    const li=document.createElement("li") 
+    li.classList.add("li")
+    const updateBtn=document.createElement("button")
+    updateBtn.textContent="Update"
+    updateBtn.classList.add("update-btn")   
+    const delbtn=document.createElement("button")
+    delbtn.textContent="Delete"   
+    delbtn.classList.add("delete-btn")
+    const completeBtn=document.createElement("button")
+    completeBtn.textContent="Complete"   
+    completeBtn.classList.add("complete-btn")
+    
+    li.innerText=Inputvalue.value
+    todoList.append(li)
+    li.append(updateBtn, delbtn , completeBtn)
+
+    delbtn.addEventListener("click",()=>{
+    li.remove()    
+    }) 
+    
+    updateBtn.addEventListener("click",()=>{
+    Inputvalue.value = cleanInput   
+    ele.textContent="Update" 
+    ele.addEventListener("click",()=>{
+        li.textContent=Inputvalue.value
+        ele.textContent="Add+" 
+        li.remove() 
+    })
+    })
+    completeBtn.addEventListener("click",()=>{
+        const newli=document.createElement("li")
+        newli.textContent= li.firstChild.textContent
+        newli.classList.add("second-li")
+        todoList.appendChild(newli)
+    })
+    Inputvalue.value=""
+}             
+}
